73b110397960ab38f6b11998732b253ac53dd462
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-02-25 14:58:58 -0400

    Add files via upload

---
+++ Frontend-ReactJS/src/components/account/Team.js
@@ -0,0 +1,111 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
+import jwt_decode from "jwt-decode";
+import { useHistory } from 'react-router-dom';
+
+const Sync = 'https://cdn.glitch.global/c58699df-f192-4af9-bca5-0e1daf50c2f9/sync-outline.png?v=1670271786767';
+
+const ReferralList = () => {
+
+    // eslint-disable-next-line
+    const [userId, setUserId] = useState('');
+    // eslint-disable-next-line
+    const [user_code, setUser_Code] = useState('');
+    // eslint-disable-next-line
+    const [ref_code, setRef_Code] = useState('');
+    // eslint-disable-next-line
+    const [token, setToken] = useState('');
+    const [expire, setExpire] = useState('');
+    // eslint-disable-next-line
+    const [users, setUsers] = useState([]);
+    // eslint-disable-next-line
+    const [msg, setMsg] = useState('');
+    const history = useHistory();
+
+    useEffect((async)  => {
+        Page();
+        refreshToken();
+        getUsers();
+    }, []);
+
+    const refreshToken = async () => {
+        try {
+            const response = await axios.get('https://ikoner-node.glitch.me/token');
+            setToken(response.data.accessToken);
+            const decoded = jwt_decode(response.data.accessToken);
+            setUserId(decoded.userId);
+            setUser_Code(decoded.user_code);
+            setRef_Code(decoded.ref_code);
+            setExpire(decoded.exp);
+        } catch (error) {
+            if (error.response) {
+                history.push("/");
+            }
+        }
+    }
+
+    const axiosJWT = axios.create();
+
+    axiosJWT.interceptors.request.use(async (config) => {
+        const currentDate = new Date();
+        if (expire * 1000 < currentDate.getTime()) {
+            const response = await axios.get('https://ikoner-node.glitch.me/token');
+            config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            setToken(response.data.accessToken);
+            const decoded = jwt_decode(response.data.accessToken);
+            setUserId(decoded.userId);
+            setUser_Code(decoded.user_code);
+            setRef_Code(decoded.ref_code);
+            setExpire(decoded.exp);
+        }
+        return config;
+    }, (error) => {
+        return Promise.reject(error);
+    });
+
+     const getUsers = async () => {
+            const response = await axios.post('https://ikoner-node.glitch.me//team', {
+                 user_code: user_code,
+                 ref_code: ref_code
+             });
+           setUsers(response.data);
+        }
+
+
+   const Page = () => {
+     document.title = 'Team | Ikoner';
+    };
+
+
+   return (
+     <div className="hero has-background-grey-light is-fullheight is-fullwidth">
+           <div className="methods3">
+           <h1 className="tasks-center">Team Record</h1>
+           <img alt="sync" className="sync-red" src={Sync} onClick={getUsers} width={25}/>
+           </div>
+           <table className="table is-striped is-fullwidth">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Username</th>
+                        <th>Balance</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, index) => (
+                        <tr key={user.id}>
+                            <td>{index + 1}</td>
+                            <td>{user.username}</td>
+                            <td>{user.balance}.00 USDT</td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
+          </div>
+            
+ )	
+}
+
+export default ReferralList;
---
+++ Frontend-ReactJS/src/components/account/Wallet.js
@@ -0,0 +1,195 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { useHistory } from 'react-router-dom';
+
+
+const Wallet = () => {
+
+    const [wait, setWait] = useState(false);
+    const [confirm, setConfirm] = useState(false);
+    const [show, setShow] = useState(true);
+    const [wallet, setWallet] = useState(false);
+    const [joke, setJoke] = useState(false);
+    const [userId, setUserId] = useState('');
+    // eslint-disable-next-line
+    const [token, setToken] = useState('');
+    const [expire, setExpire] = useState('');
+    const [bs, setBs] = useState('');
+    // eslint-disable-next-line
+    const [users, setUsers] = useState([]);
+    // eslint-disable-next-line
+    const [address, setAddress] = useState('');
+    const [addresses, setAddresses] = useState('');
+    // eslint-disable-next-line
+    const [msg, setMsg] = useState('');
+    const history = useHistory();
+
+    useEffect(() => {
+        Page();
+        refreshToken();
+    }, []);
+
+    const refreshToken = async () => {
+        try {
+            const response = await axios.get('https://ikoner-node.glitch.me/token');
+            setToken(response.data.accessToken);
+            const decoded = jwt_decode(response.data.accessToken);
+            setUserId(decoded.userId);
+            setExpire(decoded.exp);
+            setAddress(decoded.address);
+            const str = decoded.address;
+            // eslint-disable-next-line
+            String.prototype.replaceAt = function(index, replacement) {
+            if (index >= this.length) {
+            return this.valueOf();}
+            return this.substring(0, index) + replacement + this.substring(index + 20);}
+            const strBs = await str.replaceAt(7, '••••••••••••••••••••');
+            setBs(strBs);
+            if(decoded.address === null) {
+            return setWallet(false);
+            } else {
+              setWallet(true);
+            }
+        } catch (error) {
+            if (error.response) {
+                history.push("/");
+            }
+        }
+    }
+
+    const axiosJWT = axios.create();
+
+    axiosJWT.interceptors.request.use(async (config) => {
+        const currentDate = new Date();
+        if (expire * 1000 < currentDate.getTime()) {
+            const response = await axios.get('https://ikoner-node.glitch.me/token');
+            config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            setToken(response.data.accessToken);
+            const decoded = jwt_decode(response.data.accessToken);
+            setUserId(decoded.userId);
+            setAddress(decoded.address);
+            setExpire(decoded.exp);
+        }
+        return config;
+    }, (error) => {
+        return Promise.reject(error);
+    });
+
+   const SetAddress = async (e) => {
+        e.preventDefault();
+        try {
+            setWait(true);
+            const res = await axios.post('https://ikoner-node.glitch.me/address', {
+                userId: userId,
+                address: addresses
+            });
+            setJoke(true);
+            setMsg(res.data.msg);
+            setTimeout(() => {
+            history.push("/account");
+            }, 2000);  
+        } catch (error) {
+            if (error.response) {
+                setWait(false);
+                setJoke(true);
+                setMsg(error.response.data.msg);
+                setTimeout(() => {
+                setJoke(false)
+                }, 2000);
+            }
+        }
+    }
+
+
+    const onHide = () => {
+        setShow(false);
+        setConfirm(true);
+    }
+
+    const onShow = () => {
+        setConfirm(false);
+        setShow(true);
+    }
+
+
+   const Page = () => {
+     document.title = 'Wallet | Ikoner';
+    };
+
+
+    const Alert = () => {
+
+       return (
+         <div className="div-alert-msg">
+               <div className="box-alert-msg">
+                   <h1 className="alert-msg" placeholder="">{msg}</h1>
+               </div>
+         </div>
+       )
+    }
+
+ 
+    const Confirm = () => { 
+
+      return (
+
+        <div className="hero has-background-grey-light is-fullheight is-fullwidth">
+            { joke && <Alert/> }
+           <form onSubmit={SetAddress} className="box coding-gor">
+                    <div>
+                        <label className="label">Confirm Operation</label>
+                    </div>
+                      <hr className="dotted"/>
+                    <div>
+                        <input className="input" placeholder="Paste Address" value={addresses} onChange={(e) => setAddresses(e.target.value)}/>
+                    </div>
+                      <hr className="dotted"/>
+                    <div className="btn-position">
+                        <button className="button is-danger" disabled={wait} >
+                         Confirm
+                        </button>
+                           <button className="space-mg"></button>
+                        <button className="button is-danger" onClick={onShow}>
+                         Cancel
+                        </button>
+                    </div>
+                </form>
+        </div> 
+    )
+}
+ 
+    const OnAddress = () => {
+
+       return (
+
+         <div className="hero has-background-grey-light is-fullheight is-fullwidth">
+              <div className='bigpapa box-animation'>
+              <h1 className="wallet-text">Wallet Address</h1>
+             <div className="container-wallet">   
+              <div className="separater-4"></div>
+                <h1 className="">
+                 { wallet ? bs : 'No set wallet address yet' }
+                </h1>
+              </div>
+              { !wallet && 
+                <button className="button is-red" onClick={onHide}>
+                  Set Wallet Address
+                </button> }
+            </div>       
+         </div>
+    )
+}
+ 
+     return(
+
+       <div>
+         { show && <OnAddress/>}
+         { confirm && <Confirm/>}
+       </div>
+    )
+}
+
+export default Wallet
