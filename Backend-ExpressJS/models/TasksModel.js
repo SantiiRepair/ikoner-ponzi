df513fb90a429b35bbffc4dd0ab199c1c40f5ec9
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-02-25 15:20:49 -0400

    Add files via upload

---
+++ Backend-ExpressJS/models/TasksModel.js
@@ -0,0 +1,27 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const Tasks = db.define('tasks',{
+    username:{
+        type: DataTypes.STRING
+    },
+    level:{
+        type: DataTypes.STRING
+    },
+    amountTask:{
+        type: DataTypes.STRING
+    },
+    status:{
+        type: DataTypes.STRING
+    },
+    updatedAt:{
+        type: DataTypes.toString()
+    }
+        
+},{
+    freezeTableName:true
+});
+
+export default Tasks;
---
+++ Backend-ExpressJS/models/VipModel.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const Vip = db.define('vip',{
+    username:{
+        type: DataTypes.STRING
+    },
+    level:{
+        type: DataTypes.STRING
+    }
+    
+},{
+    freezeTableName:true
+});
+
+export default Vip;
