29983ac368aba41fd5da44f25816047bd2778120
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-02-27 14:15:52 -0400

    Update Database.js

--- Backend-ExpressJS/config/Database.js
+++ Backend-ExpressJS/config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
 const db = new Sequelize('ikoner','ikoner','ikoner1234',{
-    host: "https://db4free.net/",
+    host: "db4free.net",
     dialect: "mysql"
 });
 
