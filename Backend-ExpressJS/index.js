7eab8d7b11092413474567a694d5ca999aece7bd
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-02-27 20:32:23 -0400

    Update index.js

--- Backend-ExpressJS/index.js
+++ Backend-ExpressJS/index.js
@@ -28,5 +28,4 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT||3000);
-load.succeed('Server running at port 5000');
+app.listen(8080, ()=> load.succeed('Server running at port 8080'));
